a7b338e7192c0f6a60c340b1c9292978d0cdb03e
Tsubasa Ogawa <hurikake@gmail.com>
2018-01-30 11:32:15 +0900

    fix coding style

--- extract.py
+++ extract.py
@@ -2,47 +2,59 @@
 
 import os
 import re
-import sys, codecs
+import sys
+import codecs
 import tempfile
 
-# regex patterns
-regex = {
-  'separator': r'^-----',
-  'body': r'^BODY:',
-  'html': r'<[^>]+>',
-  'uri': r'https?://[a-zA-Z0-9!#$%&-~+,.?/_]+',
-  'blank_line': r'$^' # no match
-}
 
-# Remove it from text if True
-remove_flags = {
-  'html': True,
-  'uri': True,
-  'blank_line': True
-}
+class Config:
+  def __init__(self):
+    # regex patterns
+    regex = {
+      'separator': r'^-----',
+      'body': r'^BODY:',
+      'html': r'<[^>]+>',
+      'uri': r'https?://[a-zA-Z0-9!#$%&-~+,.?/_]+',
+      'blank_line': r'$^'  # no match
+    }
 
-# compile regexes
-patterns = {}
-for key in regex.keys():
-  patterns[key] = re.compile(regex[key])
+    # Remove it from text if True
+    self.remove_flags = {
+      'html': True,
+      'uri': True,
+      'blank_line': True
+    }
+
+    # compile regexes
+    self.patterns = {}
+    for key in regex.keys():
+      self.patterns[key] = re.compile(regex[key])
+
+  def get_remove_flags(self):
+    return self.remove_flags
+
+  def get_patterns(self):
+    return self.patterns
+
 
 class HBETExtractor:
   def __init__(self, file):
     self.file = file
+    self.config = Config()
 
   def extract(self):
     body_flags = {'separator': False, 'body': False}
-    return_items = []
-    item = []
+    return_items = item = []
+    patterns = self.config.get_patterns()
+
     with open(self.file) as f:
       for line in f:
         # check if last line included 'BODY:'
-        if body_flags['separator'] == True and body_flags['body'] == True:
+        if body_flags['separator'] is True and body_flags['body'] is True:
           # BODY section ends
           if patterns['separator'].match(line):
-            body_flags['separator'] = False
-            body_flags['body'] = False
-            return_item = self.__remove_waste_chars(''.join(item))
+            body_flags['separator'] = body_flags['body'] = False
+            return_item = self._remove_waste_chars(''.join(item))
             return_items.append(return_item)
             item = []
             continue
@@ -51,14 +63,14 @@ class HBETExtractor:
             item.append(line)
 
         # Header section check
-        elif body_flags['separator'] == False and body_flags['body'] == False:
+        elif body_flags['separator'] is False and body_flags['body'] is False:
           # start separator?
           if patterns['separator'].match(line):
             body_flags['separator'] = True
             continue
 
         # last line was separator?
-        elif body_flags['separator'] == True:
+        elif body_flags['separator'] is True:
           # This line includes 'BODY'?
           if patterns['body'].match(line):
             body_flags['body'] = True
@@ -67,20 +79,21 @@ class HBETExtractor:
 
     return return_items
 
-  def __remove_waste_chars(self, line):
+  def _remove_waste_chars(self, line):
+    remove_flags = self.config.get_remove_flags()
     for pattern in remove_flags.keys():
       if remove_flags[pattern]:
         line = patterns[pattern].sub('', line)
 
     return line
 
+
 def usage_and_exit():
   sys.stderr.write("usage: %s [input file] > [output file]\n" % sys.argv[0])
   sys.exit(1)
 
 if __name__ == '__main__':
-  temp_file = None
-  input_file = None
+  temp_file = input_file = None
 
   # args check
   if len(sys.argv) == 1:
